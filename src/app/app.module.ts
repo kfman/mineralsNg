6740704a7a8b9390469f9366650ea9ed29fb1a5d
KFischer (mini) <klaus.fischer@eloware.com>
2021-12-19 10:41:50 +0100

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,15 @@ import {AngularFireAuth} from '@angular/fire/compat/auth';
 import {AngularFireDatabase} from '@angular/fire/compat/database';
 import {LoginComponent} from './login/login.component';
 import {RouterModule} from '@angular/router';
-import { NavigationBarComponent } from './widgets/navigation-bar/navigation-bar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import {NavigationBarComponent} from './widgets/navigation-bar/navigation-bar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {SignUpComponent} from './sign-up/sign-up.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {provideFirebaseApp, getApp, initializeApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [
@@ -34,6 +37,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    provideFirebaseApp(() => initializeApp(firebaseSecret)),
+    provideFirestore(() => getFirestore()),
 
   ],
   providers: [
