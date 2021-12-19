61c2e267c7552483b002150660910ed505aed812
Klaus Fischer <klaus.fischer@eloware.com>
2021-12-19 19:03:00 +0100

    Getting Sample from QueryDocument

--- src/app/models/Sample.ts
+++ src/app/models/Sample.ts
@@ -1,5 +1,3 @@
-import firebase from 'firebase/compat';
-import DocumentData = firebase.firestore.DocumentData;
 import {QueryDocumentSnapshot} from '@angular/fire/compat/firestore';
 
 export class Sample {
@@ -18,9 +16,20 @@ export class Sample {
   public value: number = 0;
 
   public static fromDocument(item: QueryDocumentSnapshot<Sample>): Sample {
-    let result =  new Sample();
+    let result = new Sample();
     result.id = item.id;
+    result.sampleNumber = item.get('sampleNumber');
+    result.analytics = item.get('analytics');
+    result.annotation = item.get('annotation');
+    result.imageName = item.get('imageName');
+    result.location = item.get('location');
     result.mineral = item.get('mineral');
+    result.origin = item.get('origin');
+    result.serial = item.get('serial');
+    result.sideMineral = item.get('sideMineral');
+    result.size = item.get('size');
+    result.timeStamp = item.get('timeStamp');
+    result.value = item.get('value');
     return result;
   }
 
