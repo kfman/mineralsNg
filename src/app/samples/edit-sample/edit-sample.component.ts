9f9646dd531283f2d49954c186310e0718e5f9f6
KFischer <klaus.fischer@eloware.com>
2021-12-26 16:35:08 +0100

    Added edit sample form

--- src/app/samples/edit-sample/edit-sample.component.ts
+++ src/app/samples/edit-sample/edit-sample.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/compat/firestore';
 import {ActivatedRoute} from '@angular/router';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {Sample} from '../../models/Sample';
+import firebase from 'firebase/compat';
+import QueryDocumentSnapshot = firebase.firestore.QueryDocumentSnapshot;
 
 @Component({
   selector: 'app-edit-sample',
@@ -9,13 +13,22 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class EditSampleComponent implements OnInit {
 
-  constructor(private firestore: AngularFirestore, private route: ActivatedRoute) {
+  public sample: Sample | null = null;
+
+  constructor(private firestore: AngularFirestore, private route: ActivatedRoute,
+              private auth: AngularFireAuth) {
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(p => {
       let id = p['id'];
-      console.log(id);
+      this.auth.user.subscribe((value => {
+        const userId = value?.uid;
+        this.firestore.collection('user').doc(userId)
+          .collection('samples').doc(id).get().subscribe((value) => {
+          this.sample = Sample.fromDocument(value as QueryDocumentSnapshot<Sample>);
+        });
+      }));
     });
   }
 
